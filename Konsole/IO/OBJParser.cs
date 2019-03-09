f88980af9f9db916ba8661ec44bc8067988c7ea5
Jacob <kozilord@gmail.com>
2019-03-09 12:16:27 +0100

    Super EPIC optimised .obj parser

---
+++ Konsole/IO/OBJParser.cs
@@ -0,0 +1,99 @@
+﻿using Konsole.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konsole.IO
+{
+    public static class OBJParser
+    {
+        public static Triangle[] ParseFile(string path)
+        {
+            //Stopwatch watch = new Stopwatch();
+            //watch.Start();
+            int pIndex = 0;
+            int nIndex = 0;
+            int fIndex = 0;
+            StreamReader file = new StreamReader(path);
+            var read = true;
+            while (read)
+            {
+                var t = file.ReadLine();
+                if (t == null)
+                    read = false;
+                else if (t.StartsWith("v "))
+                    pIndex++;
+                else if (t.StartsWith("vn"))
+                    nIndex++;
+                else if (t.StartsWith("f"))
+                    fIndex++;
+                
+            }
+            
+            Vector3[] Positions = new Vector3[pIndex];
+            Vector3[] Normals = new Vector3[nIndex];
+            Triangle[] Triangles = new Triangle[fIndex];
+            //Console.WriteLine($"Counting has finished in {watch.ElapsedMilliseconds}ms.");
+            pIndex = 0;
+            nIndex = 0;
+            fIndex = 0;
+            file.BaseStream.Position = 0;
+            read = true;
+            while (read)
+            {
+                var t = file.ReadLine();
+                if (t == null)
+                    read = false;
+                else if (t.StartsWith("vn"))
+                {
+                    string[] temp = t.Split(' ');
+                    Normals[nIndex] = new Vector3(
+                        float.Parse(temp[1]),
+                        float.Parse(temp[2]),
+                        float.Parse(temp[3]));
+                    nIndex++;
+                    temp = null;
+                }
+                else if (t.StartsWith("v"))
+                {
+                    string[] temp = t.Split(' ');
+                    Positions[pIndex] = new Vector3(
+                        float.Parse(temp[1]),
+                        float.Parse(temp[2]),
+                        float.Parse(temp[3]));
+                    pIndex++;
+                    temp = null;
+                }
+
+                else if (t.StartsWith("f"))
+                {
+                    string[] Indices = t.Substring(2).Split(new char[] { ' ', '/' });
+                    int[] Temp = new int[9];
+                    for (int i = 0; i < 9; i++)
+                    {
+                        int.TryParse(Indices[i], out Temp[i]);
+                        Temp[i]--;
+                    }
+
+                    Triangles[fIndex] = new Triangle(
+                        new Vertex(Positions[Temp[0]], Normals[Temp[2]]),
+                        new Vertex(Positions[Temp[3]], Normals[Temp[5]]),
+                        new Vertex(Positions[Temp[6]], Normals[Temp[8]])
+                    );
+                    fIndex++;
+                    Temp = null;
+                }
+                
+            }
+            file.Close();
+            //watch.Stop();           
+            //Console.WriteLine($"Parsing has finished in {watch.ElapsedMilliseconds}");
+
+            return Triangles;
+        }
+    }
+}
