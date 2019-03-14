b7e9ef2e66a136b928260efc91d50acc2bd596ae
Jacob <kozilord@gmail.com>
2019-03-14 17:01:10 +0100

    ZBuffer

--- Konsole/Graphics/Drawables/Mesh.cs
+++ Konsole/Graphics/Drawables/Mesh.cs
@@ -1,9 +1,14 @@
 ﻿using Konsole.Graphics.Primitives;
+using System.Drawing;
 
 namespace Konsole.Graphics.Drawables
 {
     public class Mesh
     {
         public Triangle[] Triangles;
+        /// <summary>
+        /// TEMPORARY WORKAROUND, THIS NEEDS TO BE CHANGED.
+        /// </summary>
+        public Color Colour;
     }
 }
--- Konsole/Graphics/Rendering/FrameBuffer.cs
+++ Konsole/Graphics/Rendering/FrameBuffer.cs
@@ -5,6 +5,7 @@ using Konsole.Graphics.Primitives;
 using Konsole.IO;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Numerics;
@@ -16,7 +17,10 @@ namespace Konsole.Graphics.Rendering
     public class FrameBuffer
     {
         private readonly StringBuilder output = new StringBuilder();
+
         public Charsel[,] Buffer { get; private set; }
+        public float?[,] ZBuffer { get; private set; }
+
         private readonly Action<string> consoleWriter;
 
         private readonly List<Drawable> drawables = new List<Drawable>();
@@ -65,13 +69,27 @@ namespace Konsole.Graphics.Rendering
             {
                 Mesh = new Mesh
                 {
-                    Triangles = OBJParser.ParseFile(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + Path.DirectorySeparatorChar + "player.obj", Properties.FlipY)
+                    Triangles = OBJParser.ParseFile(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + Path.DirectorySeparatorChar + "player.obj", Properties.FlipY),
+                    Colour = Color.White
                 },
                 Scale = new Vector3(0.33f),
-                Position = new Vector3(0,0,2),
-                Origin = new Vector3(0,1.2f,0)
+                Position = new Vector3(0,0,1),
+                Origin = new Vector3(0,1.2f,0),
+                Rotation = new Vector3(MathF.PI / 2,0,0)
+            };
+            Drawable s = new Drawable
+            {
+                Mesh = new Mesh
+                {
+                    Triangles = OBJParser.ParseFile(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + Path.DirectorySeparatorChar + "shrek.obj", Properties.FlipY),
+                    Colour = Color.FromArgb(123, 186, 46)
+                },
+                Scale = new Vector3(0.60f),
+                Position = new Vector3(0, 0, 1.5f),
+                Origin = new Vector3(0, 0.4f, 0),
             };
             drawables.Add(d);
+            drawables.Add(s);
         }
 
         private float time;
@@ -83,6 +101,7 @@ namespace Konsole.Graphics.Rendering
             if (bufferInvalid)
             {
                 Buffer = new Charsel[Height, Width];
+                ZBuffer = new float?[Height, Width];
                 output.Append("\u001b[?25l");
                 projectionMatrix = Matrix4x4.CreatePerspectiveFieldOfView(1.5708f, Width / (float) Height, 1, float.PositiveInfinity);
                 bufferInvalid = false;
@@ -93,46 +112,56 @@ namespace Konsole.Graphics.Rendering
                 Char = ' ',
                 Colour = Color.White,
             });
+            ZBuffer.Populate(null);
 
+            drawables[1].Position = new Vector3(0, 0, MathF.Sin((float)clock.Time * 1.2f) + 1.2f);
             foreach (Drawable d in drawables)
             {
-                d.Rotation = new Vector3((float)clock.Time, (float)clock.Time, 0);
                 foreach (Triangle t in d.Mesh.Triangles)
                 {
-                    Vector3 pos1 = t.A.Position;
-                    Vector3 pos2 = t.B.Position;
-                    Vector3 pos3 = t.C.Position;
+                    Vector4 pos1 = new Vector4(t.A.Position, 1);
+                    Vector4 pos2 = new Vector4(t.B.Position, 1);
+                    Vector4 pos3 = new Vector4(t.C.Position, 1);
 
                     var matrix = d.DrawableMatrix;
 
                     matrix *= viewMatrix;
                     matrix *= projectionMatrix;
 
-                    pos1 = Vector3.Transform(pos1, matrix);
-                    pos2 = Vector3.Transform(pos2, matrix);
-                    pos3 = Vector3.Transform(pos3, matrix);
+                    pos1 = Vector4.Transform(pos1, matrix);
+                    pos2 = Vector4.Transform(pos2, matrix);
+                    pos3 = Vector4.Transform(pos3, matrix);
 
-                    pos1 /= pos1.Z;
-                    pos2 /= pos2.Z;
-                    pos3 /= pos3.Z;
+                    pos1 /= pos1.W;
+                    pos2 /= pos2.W;
+                    pos3 /= pos3.W;
 
-                    pos1 = pos1 * new Vector3(Width, Height / 2f, 1) + new Vector3(Width / 2f, Height / 2f, 0);
-                    pos2 = pos2 * new Vector3(Width, Height / 2f, 1) + new Vector3(Width / 2f, Height / 2f, 0);
-                    pos3 = pos3 * new Vector3(Width, Height / 2f, 1) + new Vector3(Width / 2f, Height / 2f, 0);
+                    pos1 = pos1 * new Vector4(Width, Height / 2f, 1, 1) + new Vector4(Width / 2f, Height / 2f, 0, 0);
+                    pos2 = pos2 * new Vector4(Width, Height / 2f, 1, 1) + new Vector4(Width / 2f, Height / 2f, 0, 0);
+                    pos3 = pos3 * new Vector4(Width, Height / 2f, 1, 1) + new Vector4(Width / 2f, Height / 2f, 0, 0);
 
-                    void DrawLine(Vector3 a, Vector3 b)
+                    void DrawLine(Vector4 a, Vector4 b)
                     {
-                        float k = Vector3.Distance(a, b);
+                        //We need a way to stop this from killing the performance due to the loop running way too many times in cases of long lines that go off-screen.
+                        float k = Vector4.Distance(a, b);
 
                         //perimeter
                         for (int i = 0; i < k; i++)
                         {
-                            Vector3 abLerp = Vector3.Lerp(a, b, i / k);
+                            Vector4 abLerp = Vector4.Lerp(a, b, i / k);
 
-                            bool InsideViewspace(Vector3 vec) => vec.X >= 0 && vec.X < Width && vec.Y >= 0 && vec.Y < Height;
+                            bool InsideViewspace(Vector4 vec) => vec.X >= 0 && vec.X < Width && vec.Y >= 0 && vec.Y < Height && vec.Z < 1f;
 
                             if (InsideViewspace(abLerp))
-                                Buffer[(int)abLerp.Y, (int)abLerp.X].Char = '█';
+                            {
+                                var depth = ZBuffer[(int)abLerp.Y, (int)abLerp.X];
+                                if (depth == null || depth > abLerp.Z)
+                                {
+                                    ZBuffer[(int)abLerp.Y, (int)abLerp.X] = abLerp.Z;
+                                    Buffer[(int)abLerp.Y, (int)abLerp.X].Char = '█';
+                                    Buffer[(int)abLerp.Y, (int)abLerp.X].Colour = d.Mesh.Colour;
+                                }
+                            }
                         }
                     }
 
