22a439b7d857e9be483d17fac9cdc9ffdcae9f20
Jacob <kozilord@gmail.com>
2019-03-28 16:58:58 +0100

    Experimenting

---
+++ Konsole/Entities/Components/DrawableComponent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Konsole.Entities.Components
+{
+    public class DrawableComponent : Component
+    {
+        public override long ID => (long)IDs.Drawable;
+    }
+}
